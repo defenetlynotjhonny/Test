e37bb547dd34f38a4ff84f06ffe1ac440cd6a90a
defenetlynotjhonny <krstinikola@gmail.com>
2022-10-13 18:49:06 +0200

    cleanup

--- venv/formexample.py
+++ venv/formexample.py
@@ -4,20 +4,56 @@ from flask_wtf import Form
 
 app = Flask(__name__)  
 app.secret_key ='development key'
-  
-  
-@app.route('/contact', methods = ['GET', 'POST'])  
+
+
+
+@app.route('/')  
+def index():  
+    return render_template("index.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def hello():
+    return render_template("login.html")
+
+@app.route('/contact', methods = ['GET'])  
 def contact():  
    form = ContactForm()  
    if form.validate() == False:  
       flash('All fields are required.')  
    return render_template('contact.html', form = form)  
   
+@app.route('/Customer', methods = ['GET'])
+def customer():
+   return render_template('customer.html')
+
+
+@app.route('/file_upload_form', methods = ['GET'])
+def file_upload_form():
+   return render_template('file_upload_form.html')
   
-  
+@app.route('/Logout', methods = ['GET'])
+def Logout():
+   return render_template('logout.html')
+
+@app.route('/message', methods = ['GET'])
+def message():
+   return render_template('message.html')
+
+@app.route('/profile', methods = ['GET'])
+def profile():
+   return render_template('profile.html')  
+
+@app.route('/result_data', methods = ['GET'])
+def result_data():
+   return render_template('result_dat.html')
+
 @app.route('/success',methods = ['GET','POST'])  
 def success():  
-   return render_template("success.html")  
+   return render_template("success.html") 
+
+ 
+
+ 
   
 if __name__ == '__main__':  
    app.run(debug = True)  
